bc7eaa372245537978cdd7408f8fc35dc573deaf
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-07 10:24:01 -0500

    my first hello world with springboot

---
+++ src/main/java/com/zairex/magic_cinema/HelloController.java
@@ -0,0 +1,16 @@
+package com.zairex.magic_cinema;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// restController tell to our program that is a request controller
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Hello world";
+    }
+
+
+}
