57f1c37f6f7aee28b9b13eefd2b4f5448faeacc6
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-07 10:14:50 -0500

    my first commit

---
+++ src/main/java/com/zairex/magic_cinema/MagicCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.zairex.magic_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zairex/magic_cinema/MagicCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zairex.magic_cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
