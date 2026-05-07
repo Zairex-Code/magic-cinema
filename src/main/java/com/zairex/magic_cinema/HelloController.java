a3263856f91b12115d74d08ce421fd89de0b73ef
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-07 15:39:10 -0500

    application.properties configuration

--- src/main/java/com/zairex/magic_cinema/HelloController.java
+++ src/main/java/com/zairex/magic_cinema/HelloController.java
@@ -1,19 +1,22 @@
 package com.zairex.magic_cinema;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // restController tell to our program that is a request controller
 @RestController
 public class HelloController {
+    private final String platform;
     private final MagicCinemaAIService aiService;
 
-    public HelloController(MagicCinemaAIService aiService) {
+    public HelloController(@Value("${spring.application.name}") String platform, MagicCinemaAIService aiService) {
+        this.platform = platform;
         this.aiService = aiService;
     }
 
     @GetMapping("/")
     public String hello(){
-        return this.aiService.generateGreeting();
+        return this.aiService.generateGreeting(platform);
     }
 }
--- src/main/java/com/zairex/magic_cinema/MagicCinemaAIService.java
+++ src/main/java/com/zairex/magic_cinema/MagicCinemaAIService.java
@@ -1,14 +1,15 @@
 package com.zairex.magic_cinema;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface MagicCinemaAIService {
     @UserMessage("""
-            Generate a welcoming greeting to the managing movies platform called MagicCinema 
+            Generate a welcoming greeting to the managing movies platform called {{platform}}
             use less than 120 characters and use enthusiastic tone
             """)
-    String generateGreeting();
+    String generateGreeting(@V("platform") String platform);
 
 }
