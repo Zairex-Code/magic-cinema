af2afda5cdef73d1930cb0aeab87f3ea28309939
Zairex-Code <dylanyesidflorez@gmail.com>
2026-05-12 12:32:28 -0500

    movie entity created

--- src/main/java/com/zairex/magic_cinema/persistence/entity/MovieEntity.java
+++ src/main/java/com/zairex/magic_cinema/persistence/entity/MovieEntity.java
@@ -1,4 +1,61 @@
 package com.zairex.magic_cinema.persistence.entity;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "magic_cinema_movies")
 public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 150 , unique = true)
+    private  String title;
+    @Column(nullable = false, precision = 3)
+    private Integer duration;
+    @Column(nullable = false, length = 40)
+    private String genre;
+    @Column(name = "realise_date")
+    private LocalDate realiseDate;
+    @Column(precision = 3, scale = 2)
+    private BigDecimal clasification;
+    @Column(nullable = false, length = 1)
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public LocalDate getRealiseDate() {
+        return realiseDate;
+    }
+
+    public BigDecimal getClasification() {
+        return clasification;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 }
+
+
